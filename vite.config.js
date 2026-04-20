942a43be44a4e0caeeb4a04689d441af94ba3688
unknown <dhruvshenavi@gmail.com>
2026-04-21 01:14:05 +0530

    Made code deployable by adding Host confing and Making it look for allowed Host for server and Preview for loadenv

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    plugins: [react()],
+    
+    server: {
+      allowedHosts: [env.VITE_ALLOWED_HOST]
+    },
+
+    preview: {
+      allowedHosts: [env.VITE_ALLOWED_HOST]
+    }
+  })
+}
